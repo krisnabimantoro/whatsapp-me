c8ab31b0a289a4efe890e27b207b067adc764c7a
krisnabimantoro <krisnabmntr@gmail.com>
2024-04-11 11:59:29 +0700

    updated

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -12,24 +12,26 @@ export default function Home() {
   };
 
   return (
-    <div className="">
-      <div>
-        <h1>
-          WhatsApp <span>Me</span>
-        </h1>
-        <h6>Easy chat without save phone number</h6>
-        <form action="" onSubmit={handleSubmit}>
-          <button>+62</button>
-          <input
-            className="text-black"
-            type="text"
-            placeholder="type your phone number"
-            value={inputVal}
-            onChange={(e) => setInputVal(e.target.value)}
-          />
-          <button type="submit">arrrow</button>
-        </form>
-        <p>*Your data is safe, this website not save your phone number</p>
+    <div className="container">
+      <div className="flex flex-wrap h-screen w-screen">
+        <div className=" self-center mx-auto ">
+          <h1 className=" text-center text-6xl">
+            WhatsApp <span className="font-semibold text-Secondary">Me</span>
+          </h1>
+          <h6 className="text-grey font-light text-center pt-2 text-sm ">Easy chat without save phone number</h6>
+          <form action="" className="mt-9 mx-auto max-w-max bg-white rounded-xl" onSubmit={handleSubmit}>
+            <button className="text-gray-600 text-base ml-4">+62</button>
+            <input
+              className="text-base text-gray-600 w-fit ml-5 px-2 placeholder:text-sm focus:outline-none"
+              type="text"
+              placeholder="type your phone number"
+              value={inputVal}
+              onChange={(e) => setInputVal(e.target.value)}
+            />
+            <button type="submit" className="text-black  bg-red-500 ml-12 py-3 rounded-tr-xl rounded-br-xl">arrrow</button>
+          </form>
+          <p>*Your data is safe, this website not save your phone number</p>
+        </div>
       </div>
     </div>
   );
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -13,7 +13,12 @@ const config: Config = {
         "gradient-conic":
           "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
       },
+      colors: {
+        'Secondary':'#4EDC83',
+        'grey':'#E6EDED'
+      },
     },
+   
   },
   plugins: [],
 };
